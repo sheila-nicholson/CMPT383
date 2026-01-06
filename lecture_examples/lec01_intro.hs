67ed05b3e36d2f247cf9bfb944e0eb2650fa8c2e
Sheila Nicholson <aleihsdn@gmail.com>
2026-01-05 16:46:51 -0800

    intro

---
+++ lecture_examples/lec01_intro.hs
@@ -0,0 +1,11 @@
+-- to use the interative interpreter (GHCi) run ghci in terminal -> 'it' variable remembers the result of the last expression
+
+-- to run a program -> 'ghc program.hs' to compile -> ./hello.exe to execute
+-- or you can load the file directly into ghci -> open ghci, ':l program.hs' 
+-- if the file is edited, it can be reloaded via -> ':r program.hs'
+
+main = do
+    putStrLn "Hello, world!"
+    
+   
+    
