1c1dec7a262777685d54b6457b0dc64d69da49bf
Sheila Nicholson <aleihsdn@gmail.com>
2026-04-07 12:19:58 -0700

    update

--- assignments/programming_assignments/P4_301218964.hs
+++ assignments/programming_assignments/P4_301218964.hs
@@ -59,24 +59,89 @@ infer :: Env -> Expr -> InferState (Type, ConstraintSet)    -- this needs to be 
 infer g (CInt _) = return (TInt, Set.empty)
 infer g (CBool _) = return (TBool, Set.empty)
 infer g (Var x) = case Map.lookup x g of
-                    Just y -> return(y, Set.empty)                  -- if the key exists in the map return the value
-                    Nothing -> return(TError, Set.singleton CError) -- if it does not exist you must return ConstrainSet CError
+                    Just y -> return (y, Set.empty)                  -- if the key exists in the map return the value
+                    Nothing -> return (TError, Set.singleton CError) -- if it does not exist you must return ConstrainSet CError
+infer g (Plus e1 e2) = do
+    (type1, constraint1) <- infer g e1
+    (type2, constraint2) <- infer g e2
+    return (TInt, Set.insert (CEq type1 TInt) (Set.insert (CEq type2 TInt) (Set.union constraint1 constraint2)))
+infer g (Minus e1 e2) = do
+    (type1, constraint1) <- infer g e1
+    (type2, constraint2) <- infer g e2
+    return (TInt, Set.insert (CEq type1 TInt) (Set.insert (CEq type2 TInt) (Set.union constraint1 constraint2)))
+infer g (Equal e1 e2) = do
+    (type1, constraint1) <- infer g e1
+    (type2, constraint2) <- infer g e2
+    return (TBool, Set.insert (CEq type1 type2) (Set.union constraint1 constraint2))    
+infer g (ITE e1 e2 e3) = do 
+    (type1, constraint1) <- infer g e1
+    (type2, constraint2) <- infer g e2
+    (type3, constraint3) <- infer g e3
+    return (type2, Set.insert (CEq type1 TBool) (Set.insert (CEq type2 type3) (Set.union (Set.union constraint1 constraint2) constraint3)))
 infer g (Abs x e) = do 
     y <- getFreshTVar
-    (t, c) <- infer (Map.insert x y g) e    -- extend environment g with x:y
-    return (TArr y t, c)    -- returns function type y (fresh) -> t
+    (type_, constraint) <- infer (Map.insert x y g) e    -- extend environment g with x:y
+    return (TArr y type_, constraint)    -- returns function type y (fresh) -> t
+infer g (App e1 e2) = do
+    y <- getFreshTVar
+    z <- getFreshTVar
+    (type1, constraint1) <- infer g e1
+    (type2, constraint2) <- infer g e2
+    return (z, Set.insert (CEq type1 (TArr y z)) (Set.insert (CEq type2 y) (Set.union constraint1 constraint2)))
 infer g (LetIn x e1 e2) = do 
-    y <- getFreshTVar   -- we are doing a sequence of monadic computations inside InferState
-    (type1, constraintSet1) <- infer g e1
-    (type2, constraintSet2) <- infer (Map.insert x y g) e2
-    return (type2, Set.insert (CEq y type1) (Set.union constraintSet1 constraintSet2) )     -- returns type2 (as defined in CT-Let)
+    y <- getFreshTVar   
+    let g' = Map.insert x y g
+    (type1, constraint1) <- infer g' e1
+    (type2, constraint2) <- infer g' e2
+    return (type2, Set.insert (CEq y type1) (Set.union constraint1 constraint2))     -- returns type2 (as defined in CT-Let)
                                                                                             -- constraint set = {C1 U C2 U {y=type1}}
--- infer g (App Expr Expr)
 
+-- 4. Write a function inferExpr :: Expr -> (Type, ConstraintSet) that takes a FUN expression as
+-- input and produces as output the result of constraint-based typing. Hint: you need to “eval” the
+-- monadic value defined by infer.
 
+inferExpr :: Expr -> (Type, ConstraintSet)
+inferExpr e = evalState (infer Map.empty e) 1
 
+-- 5. Find a function in the Set module to implement toCstrList :: ConstraintSet -> ConstraintList.
+-- It can convert a constraint set to a constraint list.
+
+toCstrList :: ConstraintSet -> ConstraintList
+toCstrList a = Set.toList a 
+
+-- 6. Recall that a substitution is a map from type variables to types. Define the type for substitutions
+-- type Substitution = Map.Map Type Type
+-- Write a function applySub :: Substitution -> Type -> Type that takes a substitution σ and a
+-- type T as input and produces T σ as output, i.e., applying σ to T.
+
+type Substitution = Map.Map Type Type
+
+applySub :: Substitution -> Type -> Type
+applySub sub TInt = TInt
+applySub sub TBool = TBool
+applySub sub (TVar num) = case Map.lookup (TVar num) sub of
+                            Just typ -> typ
+                            Nothing -> TVar num
+applySub sub (TArr t1 t2) = TArr (applySub sub t1) (applySub sub t2)
+
+-- 7. Write a function applySubToCstrList :: Substitution -> ConstraintList -> ConstraintList
+-- that applies a substitution to a constraint list.
+
+applySubToCstrList :: Substitution -> ConstraintList -> ConstraintList
+applySubToCstrList sub ls = map (\c -> case c of
+                                    CEq t1 t2 -> CEq (applySub sub t1) (applySub sub t2)
+                                    CError -> CError) ls
+
+-- 8. Write a function composeSub :: Substitution -> Substitution -> Substitution that takes two
+-- substitutions σ1, σ2 as input and produces σ1 ◦ σ2 as output.
+
+composeSub :: Substitution -> Substitution -> Substitution
+composeSub sub1 sub2 = case (t11 t12) 
 
 main :: IO ()
 main = do
-    let result = runState getFreshTVar 1    -- initialize the global counter to 1
-    print result
+    -- let result = runState getFreshTVar 1    -- initialize the global counter to 1
+    -- print result
+    print $ inferExpr (Abs "x" (Plus (Var "x") (CInt 1)))
+
+    -- print inference
